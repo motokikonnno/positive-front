11b27a43040fde9a63c4492bdedfd07dd45a8837
Motoki <motoki610972@icloud.com>
2022-11-30 17:15:24 +0900

    フッター作成

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,16 @@
+import Image from "next/image";
+import styles from "../styles/components/Footer.module.scss";
+
 const Footer = () => {
   return (
-    <div></div>
+    <div className={styles.footerContainer}>
+      <div className={styles.snsContainer}>
+        <Image src="/icon/facebook.svg" width={32} height={32} alt="facebookのアイコン" />
+        <Image src="/icon/twitter.svg" width={32} height={32} alt="twitterのアイコン" />
+        <Image src="/icon/instagram.svg" width={32} height={32} alt="instagramのアイコン" />
+      </div>
+      <div className={styles.text}><big className={styles.symbol}>&copy;</big> 2022 All Rights Reserved, GDO</div>
+    </div>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Header from "../components/Header";
 import Image from "next/image";
 import styles from "../styles/pages/Home.module.scss";
+import Footer from "../components/Footer";
 
 const Home: NextPage = () => {
   return (
@@ -80,6 +81,7 @@ const Home: NextPage = () => {
             </div>
           </div>
       </div>
+      <Footer />
     </>
   );
 };
