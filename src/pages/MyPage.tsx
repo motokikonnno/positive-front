7e30b31ea33f9cf003eeacaae6e6273390f7760f
Motoki <motoki610972@icloud.com>
2022-12-12 08:52:03 +0900

    カレンダー追加

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -3,11 +3,20 @@ import { useState } from "react";
 import { Layout } from "../components/Layout";
 import styles from "../styles/pages/MyPage.module.scss";
 import { ProgressBar } from "../components/ProgressBar";
-import CalendarHeatmap from 'react-calendar-heatmap';
+import CalendarHeatmap from "react-calendar-heatmap";
 import "react-calendar-heatmap/dist/styles.css";
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import '@fullcalendar/common/main.css';
+import '@fullcalendar/daygrid/main.css';
 
 const MyPage: NextPage = () => {
   const [step, setStep] = useState(18);
+  const [isShow, setIsShow] = useState(false);
+
+  const handleView = () => {
+    setIsShow(!isShow);
+  };
 
   return (
     <Layout>
@@ -22,12 +31,23 @@ const MyPage: NextPage = () => {
         <CalendarHeatmap
           startDate={new Date("2022-01-01")}
           endDate={new Date("2022-12-31")}
-
           values={[
             { date: "2022-12-11", count: 1 },
             { date: "2022-12-14", count: 2 },
           ]}
+          classForValue={(value) => {
+            if (!value) {
+              return "color-empty";
+            }
+            return `color-github-${value.count}`;
+          }}
         />
+        <div className={styles.calendarButton} onClick={handleView}>
+          カレンダー
+        </div>
+        {isShow && <div className={styles.calendarBalloon}>
+          <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" locale="ja" />
+          </div>}
       </div>
     </Layout>
   );
